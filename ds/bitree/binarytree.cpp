aaa40f2d067d9d7118f94dee4d4d91f0920bb666
Bryce Zhao <regrette.86@gmail.com>
2013-03-27 16:21:02 +0800

    do

--- ds/bitree/binarytree.cpp
+++ ds/bitree/binarytree.cpp
@@ -1,4 +1,234 @@
 #include <iostream>
-#include "BinaryTree.h"
+#include "binarytree.h"
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+/* preorder traverse to building bitree */
+void CreateBiTree(BiTree &T)
+{
+	/* PreOrder: A B C # # D E # G # # F # # #
+	 
+		   	A
+		   / 
+		  B  
+		 / \
+	    C   D
+		   / \
+		  E   F
+		   \
+		    G
+	 */
+
+	char ch;	
+
+	cin >> ch;
+
+	if (ch == '#')
+		T = NULL;
+	else
+	{
+		if (!(T = (BiTNode *)malloc(sizeof(BiTNode))))	
+			exit (OVERFLOW);
+
+		T->data = ch;
+		CreateBiTree(T->lchild);
+		CreateBiTree(T->rchild);
+	}
+}
+
+/* recursion traverse */
+void PreOrderTraverse(BiTree T /* (* visit)(TElemType e) */)
+{
+	if (T)	
+	{
+		cout << T->data;
+
+		PreOrderTraverse(T->lchild);
+		PreOrderTraverse(T->rchild);
+	}
+}
+
+void InOrderTraverse(BiTree T)
+{
+	if (T)
+	{
+		InOrderTraverse(T->lchild);
+
+		cout << T->data;
+
+		InOrderTraverse(T->rchild);
+	}
+}
+
+void PostOrderTraverse(BiTree T)
+{
+	if (T)
+	{
+		PostOrderTraverse(T->lchild);
+
+		PostOrderTraverse(T->rchild);
+
+		cout << T->data;
+	}
+}
+
+/* levelorder traverse */
+void LevelOrderTraverse(BiTree T)
+{
+	// use queue
+	const int N = 100;	
+	int front = 0, rear = 0;
+	BiTree SqQueue[N], p;
+
+	if (!T)
+		return;
+
+	SqQueue[rear] = T;
+	rear = (rear + 1) % N; // rear++
+
+	while (rear != front)
+	{
+		p = SqQueue[front];	
+		front = (front + 1) % N;
+
+		cout << p->data;
+
+		if (p->lchild)
+		{
+			SqQueue[rear] = p->lchild;
+			rear = (rear + 1) % N;
+		}
+
+		if (p->rchild)
+		{
+			SqQueue[rear] = p->rchild;	
+			rear = (rear + 1) % N;
+		}
+	} // while
+}
+
+/* unrecursion traverse */
+void PreOrdeerTraverseUnrec(BiTree T)
+{
+	// use stack
+	const int N = 50;
+	int top = 0;
+	BiTree stack[N], p;
+
+	p = T;
+	while (p || top > 0)
+	{
+		while (p)
+		{
+			cout << p->data;	
+			stack[top++] = p;
+			p = p->lchild;
+		}
+
+		if (top > 0)
+		{
+			p = stack[--top];
+			p = p->rchild;
+		}
+	} // while
+}
+
+void InOrderTraverseUnrec(BiTree T)
+{
+	// use stack
+	const int N = 50;
+	int top = 0;
+	BiTree stack[N], p;
+
+	p = T;
+	while (p || top > 0)
+	{
+		while (p)	
+		{
+			stack[top++] = p;	
+			p = p->lchild;
+		}
+
+		if (top > 0)
+		{
+			p = stack[--top];
+			cout << p->data;	
+			p = p->rchild;
+		}
+	} // while
+}
+
+void PostOrderTraverseUnrec(BiTree T)
+{
+	// use stack
+	const int N = 50;
+	int top = 0;
+	BiTree stack[N], p;
+
+	p = T;
+	while (p || top > 0)
+	{
+		while (p)	
+		{
+			stack[top++] = p;		
+			if (p->lchild)
+				p = p->lchild;
+			else
+				p = p->rchild;
+		}
+
+		if (top > 0)
+		{
+			p = stack[--top];
+			cout << p->data;
+
+			// when return from right child, stack[top-1]->rchild = p
+			if (top > 0 && stack[top-1]->rchild != p)
+				p = stack[top-1]->rchild;
+			else
+				p = NULL;
+		}
+	} // while
+}
+
+int main(void)
+{
+	BiTree T;
+
+	cout << "INPUT: ABC##DE#G##F###" << endl;
+	CreateBiTree(T);
+
+	cout << "PreOrderTraverse: ";
+	PreOrderTraverse(T);
+	cout << endl;
+
+	cout << "InOrderTraverse: ";
+	InOrderTraverse(T);
+	cout << endl;
+
+	cout << "PostOrderTraverse: ";
+	PostOrderTraverse(T);
+	cout << endl;
+
+	cout << "LevelOrderTraverse: ";
+	LevelOrderTraverse(T);
+	cout << endl;
+
+	cout << "PreOrdeerTraverseUnrec: ";
+	PreOrdeerTraverseUnrec(T);
+	cout << endl;
+
+	cout << "InOrderTraverseUnrec: ";
+	InOrderTraverseUnrec(T);
+	cout << endl;
+
+	cout << "PostOrderTraverseUnrec: ";
+	PostOrderTraverseUnrec(T);
+	cout << endl;
+	
+	return 0;
+}
 
 
--- ds/bitree/binarytree.h
+++ ds/bitree/binarytree.h
@@ -1,17 +1,42 @@
 #ifndef _BINARY_TREE_H_
 #define _BINARY_TREE_H_
 
+#define OVERFLOW -1
+
 typedef char TElemType;
 
-typdef struct BiTNode {
+typedef struct BiTNode {
 	TElemType data;
 	struct BiTNode *lchild, *rchild;
 }BiTNode, *BiTree;
 
+/* preorder traverse to building bitree */
 void CreateBiTree(BiTree &T);
+
+/* recursion traverse */
 void PreOrderTraverse(BiTree T /* (* visit)(TElemType e) */);
 void InOrderTraverse(BiTree T);
 void PostOrderTraverse(BiTree T);
+
+/* levelorder traverse */
 void LevelOrderTraverse(BiTree T);
+
+/* unrecursion traverse */
+void PreOrdeerTraverseUnrec(BiTree T);
+void InOrderTraverseUnrec(BiTree T);
+void PostOrderTraverseUnrec(BiTree T);
+
+/* Thread bitree traverse */
+/*
+void PreOrderTraverseThread(BiTree T);
+void InOrderTraverseThread(BiTree T);
+void PostOrderTraverseThread(BiTree T);
+*/
+
+/* transfer lchild and rchild */
+
+/* Depth */
+
+/* leaf */
 
 #endif /* _BINARY_TREE_H_ */
