509915b9fe638de6ab6cc6aa55a4fdc46c7d7444
Bryce <regrette.86@gmail.com>
2013-04-15 23:12:58 +0800

    Update linklist.cpp

--- ds/list/linklist.cpp
+++ ds/list/linklist.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-/* ������������ */
+/* 创建线性链表 */
 LinkList buildLineList()
 {
 	srand((unsigned int)time(NULL));
@@ -23,7 +23,7 @@ LinkList buildLineList()
 	return head;
 }
 
-/* ���������������� */
+/* 创建有序线性链表 */
 LinkList buildSortedLineList(int a[], int n)
 {
 	LinkList head = (LinkList)malloc(sizeof(LNode));	
@@ -39,7 +39,7 @@ LinkList buildSortedLineList(int a[], int n)
 	return head;
 }
 
-/* �������л��ĵ����� */
+/* 创建带有环的单链表 */
 LinkList buildHasCircleList(int a[], int n)
 {
 	LinkList head = (LinkList)malloc(sizeof(LNode));
@@ -61,7 +61,7 @@ LinkList buildHasCircleList(int a[], int n)
 	return head;
 }
 
-/* ����2���ཻ�ĵ����� */
+/* 创建2个相交的单链表 */
 void buildTwoIntersectList(LNode **L1, LNode **L2, int nodes)
 {
 	*L1 = (LinkList)malloc(sizeof(LNode));		
@@ -91,7 +91,7 @@ void buildTwoIntersectList(LNode **L1, LNode **L2, int nodes)
 	curr2->next = intersect;
 }
 
-/* ������������ */
+/* 遍历线性链表 */
 void TraverseLineList(LinkList &l)
 {
 	LinkList p = l;
@@ -103,7 +103,7 @@ void TraverseLineList(LinkList &l)
 	cout << endl;
 }
 
-/* ��ȡ��������ֵΪk�Ľ��� */
+/* 获取单链表中值为k的结点 */
 LinkList GetOneNodeFromList(LinkList &l, int k)
 {
 	if (l == NULL)	
@@ -120,8 +120,8 @@ LinkList GetOneNodeFromList(LinkList &l, int k)
 	return NULL;
 }
 
-/* 1. ���������н����ĸ��� */
-/* ���������������ˣ�Ӧ���ܹ�Ѹ��д����ȷ�Ĵ��룬ע�����������Ƿ�Ϊ�ա�ʱ�临�Ӷ�ΪO��n����*/
+/* 1. 求单链表中结点的个数 */
+/* 这是最最基本的了，应该能够迅速写出正确的代码，注意检查链表是否为空。时间复杂度为O（n）。*/
 int GetListNodes(LinkList &l)
 {
 	if (l == NULL || l->next == NULL)
@@ -140,37 +140,37 @@ int GetListNodes(LinkList &l)
 }
 
 
-/* 2. ����������ת */
-/* ��ͷ��β����ԭ������ÿ����һ�����㣬����ժ�·�������������ǰ�ˡ�ע������Ϊ�պ�ֻ��һ��������������ʱ�临�Ӷ�ΪO��n����*/
+/* 2. 将单链表反转 */
+/* 从头到尾遍历原链表，每遍历一个结点，将其摘下放在新链表的最前端。注意链表为空和只有一个结点的情况。时间复杂度为O（n）。*/
 LinkList ReverseList(LinkList &l)
 {
-	// ��������Ϊ�ջ�ֻ��һ�����㣬���跴ת��ֱ�ӷ���ԭ����ͷָ��
+	// 如果链表为空或只有一个结点，无需反转，直接返回原链表头指针
 	if (l == NULL || l->next == NULL)
 		return l;
 	
 	LinkList p = l;
-	LinkList q = NULL; // ��ת����������ͷָ�룬��ʼΪNULL
+	LinkList q = NULL; // 反转后的新链表头指针，初始为NULL
 	while (p != NULL)
 	{
 		LinkList temp = p;
 		p = p->next;
-		temp->next = q; // ����ǰ����ժ�£���������������ǰ��
+		temp->next = q; // 将当前结点摘下，插入新链表的最前端
 		q = temp;
 	}
 	
 	return q;
 }
 
-/* 3. ���ҵ������е�����k������ ��k>0��*/
-/* ���ձ��ķ����ǣ���ͳ�Ƶ������н����ĸ�����Ȼ�����ҵ��ڣ�n-k�������㡣ע������Ϊ�գ�kΪ0��kΪ1��k���������нڵ�����ʱ��������
- * ʱ�临�Ӷ�ΪO��n����
- * ������Ҫ��һ����һ��˼·������˼·��������Ŀ��Ҳ����Ӧ�á���Ҫ˼·����ʹ������ָ�룬����ǰ����ָ���ߵ�������k�����㣬
- * ����ǰ������ָ���ľ�������k-1��֮��ǰ������ָ��һ����ǰ�ߣ�ǰ����ָ���ߵ�����һ������ʱ��
- * ����ָ����ָ�������ǵ�����k�����㡣
+/* 3. 查找单链表中倒数第k个结点 （k>0）*/
+/* 最普遍的方法是，先统计单链表中结点的个数，然后再找到第（n-k）个结点。注意链表为空，k为0，k为1，k大于链表中节点个数时的情况。
+ * 时间复杂度为O（n）。
+ * 这里主要讲一下另一个思路，这种思路在其他题目中也会有应用。主要思路就是使用两个指针，先让前面的指针走到正向第k个结点，
+ * 这样前后两个指针的距离差是k-1，之后前后两个指针一起向前走，前面的指针走到最后一个结点时，
+ * 后面指针所指结点就是倒数第k个结点。
  */
 LinkList ReverseGetKthNode(LinkList &l, unsigned int k)
 {
-	if (l == NULL || k == 0) // ����k�ļ����Ǵ�1��ʼ�ģ���kΪ0������Ϊ�շ���NULL
+	if (l == NULL || k == 0) // 这里k的计数是从1开始的，若k为0或链表为空返回NULL
 			return NULL;
 
 	LinkList first = l;
@@ -181,32 +181,32 @@ LinkList ReverseGetKthNode(LinkList &l, unsigned int k)
 		k--;
 	}
 
-	if (k > 1 || first == NULL) // ��������С��k������NULL
+	if (k > 1 || first == NULL) // 前面的指针先走到正向第k个结点
 		return NULL;
 
-	while (first->next != NULL) // ǰ������ָ��һ����ǰ�ߣ�ֱ��ǰ����ָ��ָ������һ������    		
+	while (first->next != NULL) // 前后两个指针一起向前走，直到前面的指针指向最后一个结点 
 	{
 		first = first->next;	
 		second = second->next;
 	}
 	
-	return second; // ������ָ����ָ�������ǵ�����k������ 	
+	return second; // 后面的指针所指结点就是倒数第k个结点 
 }
 
-/* 4.���ҵ��������м����� */
-/* ������Ӧ������һ�����Ƶ�˼�롣Ҳ����������ָ�룬ֻ���������ǣ�����ָ��ͬʱ��ǰ�ߣ�ǰ����ָ��ÿ����������
- * ������ָ��ÿ����һ����ǰ����ָ���ߵ�����һ������ʱ��������ָ����ָ���������м����㣬���ڣ�n/2+1�������㡣
- * ע������Ϊ�գ�������������Ϊ1��2��������ʱ�临�Ӷ�O��n����
+/* 4.查找单链表的中间结点 */
+/* 此题可应用于上一题类似的思想。也是设置两个指针，只不过这里是，两个指针同时向前走，前面的指针每次走两步，
+ * 后面的指针每次走一步，前面的指针走到最后一个结点时，后面的指针所指结点就是中间结点，即第（n/2+1）个结点。
+ * 注意链表为空，链表结点个数为1和2的情况。时间复杂度O（n）。
  */
 LinkList GetMiddleNode(LinkList &l)
 {
-	if (l == NULL || l->next == NULL)  // ����Ϊ�ջ�ֻ��һ�����㣬����ͷָ��
+	if (l == NULL || l->next == NULL)  // 链表为空或只有一个结点，返回头指针
 		return l;
 	
 	LinkList fast = l;
 	LinkList slow = l;
 
-	while (fast != NULL && fast->next != NULL) // ǰ��ָ��ÿ����������ֱ��ָ������һ�����㣬����ָ��ÿ����һ��
+	while (fast != NULL && fast->next != NULL) // 前面指针每次走两步，直到指向最后一个结点，后面指针每次走一步
 	{
 		fast = fast->next;
 		slow = slow->next;
@@ -214,12 +214,12 @@ LinkList GetMiddleNode(LinkList &l)
 			fast = fast->next;
 	}
 
-	return slow; // ������ָ����ָ���㼴Ϊ�м�����  
+	return slow; // 后面的指针所指结点即为中间结点 
 }
 
-/* 5. ��β��ͷ���������� */
-/* �������ֵߵ�˳�������⣬����Ӧ�þͻ��뵽ջ�������ȳ������ԣ���һ��Ҫô�Լ�ʹ��ջ��Ҫô��ϵͳʹ��ջ��
- * Ҳ���ǵݹ顣ע������Ϊ�յ�������ʱ�临�Ӷ�ΪO��n����
+/* 5. 从尾到头遍历单链表 */
+/* 对于这种颠倒顺序的问题，我们应该就会想到栈，后进先出。所以，这一题要么自己使用栈，要么让系统使用栈，
+ * 也就是递归。注意链表为空的情况。时间复杂度为O（n）。
  */
 void ReversePrintListRec(LinkList &l)
 {
@@ -253,9 +253,9 @@ void ReversePrintListUnrec(LinkList &l)
 	cout << endl;
 }
 
-/* 6. ��֪����������L1��L2���������� �����Ǻϲ���һ��������Ȼ���� */
-/* �������ƹ鲢����������ע������������Ϊ�գ�������һ��Ϊ��ʱ��������ֻ��ҪO��1���Ŀռ䡣
- * ʱ�临�Ӷ�ΪO��max(len1, len2)����
+/* 6. 已知两个单链表L1和L2各自有序， 把它们合并成一个链表依然有序 */
+/* 这个类似归并排序。尤其注意两个链表都为空，和其中一个为空时的情况。只需要O（1）的空间。
+ * 时间复杂度为O（max(len1, len2)）。
  */
 LinkList MergeSortedListRec(LinkList &L1, LinkList &L2)
 {
@@ -326,35 +326,35 @@ LinkList MergeSortedListUnrec(LinkList &L1, LinkList &L2)
 	return pMerge;
 }
 
-/* 7. �ж�һ���������Ƿ��л� */
-/* ����Ҳ���õ�����ָ�롣����һ���������л���Ҳ����˵��һ��ָ��ȥ����������Զ�߲���ͷ�ġ�
- * ���ˣ����ǿ���������ָ��ȥ������һ��ָ��һ����������һ��ָ��һ����һ���������л�������ָ���϶����ڻ���������
- * ʱ�临�Ӷ�ΪO��n����
+/* 7. 判断一个单链表是否有环 */
+/* 这里也是用到两个指针。如果一个链表中有环，也就是说用一个指针去遍历，是永远走不到头的。
+ * 因此，我们可以用两个指针去遍历，一个指针一次走两步，一个指针一次走一步，如果有环，两个指针肯定会在环中相遇。
+ * 时间复杂度为O（n）。
  */
 bool ListHasCircle(LinkList &l)
 {
 	if (l == NULL || l->next == NULL)
 		return false;
 
-	LinkList fast = l; // ��ָ��ÿ��ǰ��2��
-	LinkList slow = l; // ��ָ��ÿ��ǰ��1��
+	LinkList fast = l; // 快指针每次前进2步
+	LinkList slow = l; // 慢指针每次前几1步
 
 	while (fast != NULL && fast->next != NULL)
 	{
 		fast = fast->next->next;	
 		slow = slow->next;
 
-		if (fast == slow) // ���������ڻ�
+		if (fast == slow) // 相遇，存在环
 			return true;
 	}
 	return false;
 }
 
-/* 8. �ж������������Ƿ��ཻ */
-/* �������������ཻ��ĳһ�ڵ㣬��ô�������ཻ�ڵ�֮�������нڵ㶼���������������еġ�
- * Ҳ����˵���������������ཻ����ô����һ���ڵ��϶��ǹ��еġ�
- * �ȱ�����һ����������ס����һ���ڵ㣬Ȼ�������ڶ���������������һ���ڵ�ʱ�͵�һ������������һ���ڵ����Ƚϣ�
- * ������ͬ�����ཻ���������ཻ��ʱ�临�Ӷ�ΪO(len1+len2)����Ϊֻ��Ҫһ������ָ�뱣������һ���ڵ���ַ���ռ临�Ӷ�ΪO(1)��
+/* 8. 判断两个单链表是否相交 */
+/* 如果两个链表相交于某一节点，那么在这个相交节点之后的所有节点都是两个链表所共有的。
+ * 也就是说，如果两个链表相交，那么最后一个节点肯定是共有的。
+ * 先遍历第一个链表，记住最后一个节点，然后遍历第二个链表，到最后一个节点时和第一个链表的最后一个节点做比较，
+ * 如果相同，则相交，否则不相交。时间复杂度为O(len1+len2)，因为只需要一个额外指针保存最后一个节点地址，空间复杂度为O(1)。
  */
 bool ListIsInterSection(LinkList &L1, LinkList &L2)
 {
@@ -372,12 +372,12 @@ bool ListIsInterSection(LinkList &L1, LinkList &L2)
 	return (l1 == l2);
 }
 
-/* 9. �������������ཻ�ĵ�һ������ */
-/* �Ե�һ���������������㳤��len1��ͬʱ��������һ���ڵ��ĵ�ַ��
- * �Եڶ����������������㳤��len2��ͬʱ��������һ���ڵ��Ƿ��͵�һ������������һ���ڵ���ͬ��������ͬ�����ཻ��������
- * ������������ͷ�ڵ㿪ʼ������len1����len2����ô����һ�������ȱ���len1-len2���ڵ㣬
- * ��ʱ����������ǰ�ڵ㵽��һ���ཻ�ڵ��ľ����������ˣ�Ȼ��һ������������֪�������ڵ��ĵ�ַ��ͬ��
- * ʱ�临�Ӷȣ�O(len1+len2)��
+/* 9. 求两个单链表相交的第一个结点 */
+/* 对第一个链表遍历，计算长度len1，同时保存最后一个节点的地址。
+ * 对第二个链表遍历，计算长度len2，同时检查最后一个节点是否和第一个链表的最后一个节点相同，若不相同，不相交，结束。
+ * 两个链表均从头节点开始，假设len1大于len2，那么将第一个链表先遍历len1-len2个节点，
+ * 此时两个链表当前节点到第一个相交节点的距离就相等了，然后一起向后遍历，知道两个节点的地址相同。
+ * 时间复杂度，O(len1+len2)。
  */
 LinkList GetListFirstIntersectNode(LinkList &L1, LinkList &L2)
 {
@@ -400,12 +400,12 @@ LinkList GetListFirstIntersectNode(LinkList &L1, LinkList &L2)
 		len2++;
 	}
 
-	if (pNode1 != pNode2) // ���ֱཻ�ӷ���NULL
+	if (pNode1 != pNode2) // 不相交直接返回NULL
 		return NULL;
 
 	pNode1 = L1;
 	pNode2 = L2;
-	// �ȶ������������ĵ�ǰ���㣬ʹ֮��β�ڵ��ľ�������
+	// 先对齐两个链表的当前结点，使之到尾节点的距离相等
 	if (len1 > len2)
 	{
 		int k = (len1 - len2);	
@@ -428,9 +428,9 @@ LinkList GetListFirstIntersectNode(LinkList &L1, LinkList &L2)
 	return pNode1;	
 }
 
-/* 10. ��֪һ�����������ڻ��� �����뻷�еĵ�һ������ */
-/* �����ж��Ƿ����ڻ����������ڽ������ڻ��е�һ���ڵ㴦�Ͽ�����Ȼ��������ʱ�����ƻ�ԭ��������
- * �������γ��������ཻ�ĵ������������뻷�еĵ�һ���ڵ�Ҳ��ת�������������������ཻ�ĵ�һ���ڵ㡣
+/* 10. 已知一个单链表存在环， 求进入环中的第一个结点 */
+/* 首先判断是否存在环，若不存在结束。在环中的一个节点处断开（当然函数结束时不能破坏原链表），
+ * 这样就形成了两个相交的单链表，求进入环中的第一个节点也就转换成了求两个单链表相交的第一个节点。
  */ 
 LinkList GetFirstNodeIntoCircle(LinkList &l)
 {
@@ -450,7 +450,7 @@ LinkList GetFirstNodeIntoCircle(LinkList &l)
 	if (fast == NULL || fast->next == NULL)
 		return NULL;
 
-	// �����еĴ˽ڵ���Ϊ������β�ڵ㣬�������������������ཻ����
+	// 将环中的此节点作为假设的尾节点，将它变成两个单链表相交问题
 	LinkList pAssumedTail = slow;
 	LinkList pNode1 = l;
 	LinkList pNode2 = pAssumedTail->next;
@@ -470,7 +470,7 @@ LinkList GetFirstNodeIntoCircle(LinkList &l)
 
 	pNode1 = l;
 	pNode2 = pAssumedTail->next;
-	// �ȶ������������ĵ�ǰ���㣬ʹ֮��β�ڵ��ľ�������
+	// 先对齐两个链表的当前结点，使之到尾节点的距离相等
 	if (len1 > len2)
 	{
 		int k = len1 - len2;	
@@ -493,11 +493,11 @@ LinkList GetFirstNodeIntoCircle(LinkList &l)
 	return pNode1;
 }
 
-/* 11. ����һ������ָ��pHead��һ����ָ��pToBeDeleted��O(1)ʱ�临�Ӷ�ɾ������pToBeDeleted */
-/* ����ɾ���ڵ㣬������ͨ��˼·�����øýڵ���ǰһ���ڵ�ָ���ýڵ�����һ���ڵ㣬����������Ҫ�����ҵ��ýڵ���ǰһ���ڵ㣬
- * ʱ�临�Ӷ�ΪO(n)�����������������е�ÿ���ڵ��ṹ����һ���ģ��������ǿ��԰Ѹýڵ�����һ���ڵ������ݸ��Ƶ��ýڵ㣬
- * Ȼ��ɾ����һ���ڵ㼴�ɡ�Ҫע������һ���ڵ�������������ʱ��ֻ���ó����ķ�����������
- * ���ҵ�ǰһ���ڵ㣬��������ƽ��ʱ�临�ӶȻ���O(1)��
+/* 11. 给出一单链表指针pHead和一结点指针pToBeDeleted，O(1)时间复杂度删除结点pToBeDeleted */
+/* 对于删除节点，我们普通的思路就是让该节点的前一个节点指向该节点的下一个节点，这种情况需要遍历找到该节点的前一个节点，
+ * 时间复杂度为O(n)。对于链表，链表中的每个节点结构都是一样的，所以我们可以把该节点的下一个节点的数据复制到该节点，
+ * 然后删除下一个节点即可。要注意最后一个节点的情况，这个时候只能用常见的方法来操作，
+ * 先找到前一个节点，但总体的平均时间复杂度还是O(1)。
  */
 void DeleteOneNode(LinkList &l, LinkList &pToBeDeleted)
 {
@@ -506,14 +506,14 @@ void DeleteOneNode(LinkList &l, LinkList &pToBeDeleted)
 
 	if (pToBeDeleted->next != NULL)
 	{
-		pToBeDeleted->data = pToBeDeleted->next->data; // ����һ���ڵ������ݸ��Ƶ����ڵ㣬Ȼ��ɾ����һ���ڵ�
+		pToBeDeleted->data = pToBeDeleted->next->data; // 将下一个节点的数据复制到本节点，然后删除下一个节点
 		LinkList temp = pToBeDeleted->next;
 		pToBeDeleted->next = pToBeDeleted->next->next;
 		delete temp;
 	}
-	else // Ҫɾ����������һ���ڵ� 
+	else // 要删除的是最后一个节点 
 	{
-		if (l == pToBeDeleted) // ������ֻ��һ���ڵ�������  
+		if (l == pToBeDeleted) // 链表中只有一个节点的情况 
 		{
 			l = pToBeDeleted;		
 			delete pToBeDeleted;
@@ -521,7 +521,7 @@ void DeleteOneNode(LinkList &l, LinkList &pToBeDeleted)
 		else
 		{
 			LinkList pNode = l;		
-			while (pNode->next != pToBeDeleted) // �ҵ������ڶ����ڵ�
+			while (pNode->next != pToBeDeleted) // 找到倒数第二个节点
 				pNode = pNode->next;
 			pNode->next = NULL;
 			delete pToBeDeleted;
